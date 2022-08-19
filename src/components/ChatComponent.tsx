b5570a7849865adc9786a1cbcc44eaf579a83cb5
Joel Rubin <joelsrubin@joelrubxqw3vh0k.lan>
2022-08-19 12:45:35 -0500

    updated

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -43,84 +43,90 @@ const AblyChatComponent = () => {
   return (
     <div className="text-md flex h-screen flex-col font-mono">
       {session ? (
-        <div className="flex h-full flex-col overflow-auto">
-          <Nav>
-            <div className="flex flex-row justify-between">
-              <p className="flex items-center justify-center">
-                hi {session.user?.name}
-              </p>
-              <h1 className="bg-gray-800 pt-4 text-center underline decoration-slate-400">
-                Chats
-              </h1>
-
-              <button onClick={() => signOut()}>Sign out</button>
-            </div>
-          </Nav>
-          <main className="text-md overflow-scroll font-mono">
-            <div className="flex flex-col justify-between gap-4 py-10">
-              {receivedMessages.map((message, i) => {
-                return (
-                  <div
-                    className={`mx-4 flex h-full w-fit flex-col justify-between ${
-                      USER_NAME === message.author && "self-end "
-                    }`}
-                    key={i}
-                  >
+        <>
+          <div className="flex h-full flex-col overflow-auto">
+            <Nav>
+              <div className="flex flex-row justify-between">
+                <p className="flex items-center justify-center">
+                  hi {session.user?.name}
+                </p>
+                <h1 className="bg-gray-800 pt-4 text-center underline decoration-slate-400">
+                  Chats
+                </h1>
+                <button onClick={() => signOut()}>Sign out</button>
+              </div>
+            </Nav>
+            <main className="text-md overflow-scroll font-mono">
+              <div className="flex flex-col justify-between gap-4 py-10">
+                {receivedMessages.map((message, i) => {
+                  return (
                     <div
-                      className={`${
-                        USER_NAME === message.author
-                          ? "self-end rounded-l-lg rounded-br-lg bg-blue-900 text-right"
-                          : "rounded-r-lg rounded-bl-lg bg-green-900"
-                      } p-4`}
-                    >
-                      <p>{message.text}</p>
-                    </div>
-                    <p
-                      className={`pt-2 text-xs ${
-                        USER_NAME === message.author && "self-end"
+                      className={`mx-4 flex h-full w-fit flex-col justify-between ${
+                        USER_NAME === message.author && "self-end "
                       }`}
+                      key={i}
                     >
-                      {message.author}
-                    </p>
-                  </div>
-                );
-              })}
-              <div ref={boxRef}></div>
-            </div>
-          </main>
-        </div>
+                      <div
+                        className={`${
+                          USER_NAME === message.author
+                            ? "self-end rounded-l-lg rounded-br-lg bg-blue-900 text-right"
+                            : "rounded-r-lg rounded-bl-lg bg-green-900"
+                        } p-4`}
+                      >
+                        <p>{message.text}</p>
+                      </div>
+                      <p
+                        className={`pt-2 text-xs ${
+                          USER_NAME === message.author && "self-end"
+                        }`}
+                      >
+                        {message.author}
+                      </p>
+                    </div>
+                  );
+                })}
+                <div ref={boxRef}></div>
+              </div>
+            </main>
+          </div>
+          <footer className="flex w-full justify-center bg-gray-800">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                //@ts-ignore
+                channel?.publish("test", messageText);
+                setMessageText("");
+                inputRef.current?.focus();
+              }}
+              className="m-10 flex w-3/4 flex-col justify-center font-mono text-lg sm:w-1/2 md:w-1/4"
+            >
+              <input
+                type="text"
+                onChange={(e) => {
+                  setMessageText(e.target.value);
+                }}
+                className="rounded-sm p-2 pl-4 text-black"
+                placeholder="Type a message"
+                value={messageText}
+                autoFocus
+                ref={inputRef}
+              />
+              <button className="mt-2 rounded-md bg-slate-200 p-2 text-black shadow-md shadow-black active:translate-y-0.5 active:shadow-none">
+                send
+              </button>
+            </form>
+          </footer>
+        </>
       ) : (
         <div className="align-center flex h-full items-center justify-center text-center">
-          <button onClick={() => signIn("discord")}>Log in with discord</button>
+          <button
+            onClick={() => signIn("discord")}
+            className="rounded-sm bg-slate-200 p-4 text-black active:translate-y-0.5 active:shadow-none"
+          >
+            Log in with discord
+          </button>
         </div>
       )}
-      <footer className="flex w-full justify-center bg-gray-800">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            //@ts-ignore
-            channel?.publish("test", messageText);
-            setMessageText("");
-            inputRef.current?.focus();
-          }}
-          className="m-10 flex w-3/4 flex-col justify-center font-mono text-lg sm:w-1/2 md:w-1/4"
-        >
-          <input
-            type="text"
-            onChange={(e) => {
-              setMessageText(e.target.value);
-            }}
-            className="rounded-sm p-2 pl-4 text-black"
-            placeholder="Type a message"
-            value={messageText}
-            autoFocus
-            ref={inputRef}
-          />
-          <button className="mt-2 rounded-md bg-slate-200 p-2 text-black shadow-md shadow-black active:translate-y-0.5 active:shadow-none">
-            send
-          </button>
-        </form>
-      </footer>
     </div>
   );
 };

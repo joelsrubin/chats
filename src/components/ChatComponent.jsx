c0ba80d2e18a777f24e20c96c5979069fdcc3043
Joel Rubin <joelsrubin@joelrubxqw3vh0k.lan>
2022-08-19 10:11:07 -0500

    add autoscroll

--- src/components/ChatComponent.jsx
+++ src/components/ChatComponent.jsx
@@ -1,11 +1,10 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useChannel } from "./ChatReactEffect";
 import Nav from "./Nav";
 
 const AblyChatComponent = () => {
-  let inputBox = null;
-  let messageEnd = null;
+  const boxRef = useRef();
 
   const [messageText, setMessageText] = useState("");
   const [receivedMessages, setMessages] = useState([]);
@@ -24,25 +23,29 @@ const AblyChatComponent = () => {
     ]);
   });
 
+  useEffect(() => {
+    boxRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [receivedMessages]);
+
   const ABLY_CLIENT_ID = ably.auth.clientId;
 
   return (
-    <div className="flex flex-col h-screen font-mono text-lg">
+    <div className="flex h-screen flex-col font-mono text-lg">
       {session ? (
-        <div className="flex flex-col h-full overflow-scroll">
+        <div className="flex h-full flex-col overflow-scroll">
           <Nav>
             <div className="flex flex-row justify-between">
-              <p className="flex justify-center items-center">
+              <p className="flex items-center justify-center">
                 hi {session.user?.name}
               </p>
-              <h1 className="text-center bg-gray-800 pt-4 decoration-slate-400 underline">
+              <h1 className="bg-gray-800 pt-4 text-center underline decoration-slate-400">
                 Chats
               </h1>
 
               <button onClick={() => signOut()}>Sign out</button>
             </div>
           </Nav>
-          <main className="font-mono text-lg overflow-scroll">
+          <main className="overflow-scroll font-mono text-lg">
             <div className="flex flex-col justify-between py-10">
               {receivedMessages.map((message, i) => {
                 return (
@@ -57,33 +60,35 @@ const AblyChatComponent = () => {
                   </div>
                 );
               })}
+              <div ref={boxRef}></div>
             </div>
           </main>
         </div>
       ) : (
-        <div className="flex justify-center items-center text-center align-center h-full">
+        <div className="align-center flex h-full items-center justify-center text-center">
           <button onClick={() => signIn("discord")}>Log in with discord</button>
         </div>
       )}
-      <footer className="w-full flex justify-center bg-gray-800">
+      <footer className="flex w-full justify-center bg-gray-800">
         <form
           onSubmit={(e) => {
             e.preventDefault();
             channel.publish("test", messageText);
             setMessageText("");
           }}
-          className="flex flex-col w-3/4 sm:w-1/2 md:w-1/4 font-mono text-lg justify-center m-10"
+          className="m-10 flex w-3/4 flex-col justify-center font-mono text-lg sm:w-1/2 md:w-1/4"
         >
           <input
             type="text"
             onChange={(e) => {
               setMessageText(e.target.value);
             }}
-            className="text-black p-2 rounded-sm pl-4"
+            className="rounded-sm p-2 pl-4 text-black"
             placeholder="Type a message"
             value={messageText}
+            autoFocus
           />
-          <button className="p-2 mt-2 rounded-md text-black shadow-md shadow-black bg-slate-200 active:shadow-none active:translate-y-0.5">
+          <button className="mt-2 rounded-md bg-slate-200 p-2 text-black shadow-md shadow-black active:translate-y-0.5 active:shadow-none">
             send
           </button>
         </form>
